b51fd13a17ba9aa70d26cf854df96154c9528dec
Hackbright Student <info@hackbrightacademy.com>
2015-02-09 16:44:20 -0800

    Defined classes in model and added user data from u.user into ratings.db

--- model.py
+++ model.py
@@ -1,13 +1,47 @@
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
+from sqlalchemy.orm import sessionmaker
 
+ENGINE = None
+Session = None
 Base = declarative_base()
 
 ### Class declarations go here
+class User(Base):
+    __tablename__ = "users"
 
+    id = Column(Integer, primary_key = True)
+    email = Column(String(64), nullable=True)
+    password = Column(String(64), nullable=True)
+    age = Column(Integer, nullable=True)
+    zipcode = Column(String(15), nullable=True)
 
+class Movie(Base):
+    __tablename__ = "movies"
+
+    id = Column(Integer, primary_key= True)
+    name = Column(String(64))
+    released_at= Column(DateTime)
+    imdb_url = Column(String(100))
+
+class Rating(Base):
+    __tablename__ = "ratings"
+
+    id = Column(Integer, primary_key=True)
+    movie_id = Column(Integer, ForeignKey('movies.id'))
+    user_id = Column(Integer, ForeignKey('users.id'))
+    rating = Column(Integer)
 ### End class declarations
+
+def connect():
+    global ENGINE
+    global Session
+
+    ENGINE = create_engine("sqlite:///ratings.db", echo=True)
+    Session = sessionmaker(bind=ENGINE)
+
+    return Session()
 
 def main():
     """In case we need this for something"""
--- seed.py
+++ seed.py
@@ -1,9 +1,14 @@
 import model
+from model import User, Movie, Rating 
 import csv
 
 def load_users(session):
-    # use u.user
-    pass
+    with open('seed_data/u.user', 'rb') as user_file:
+        reader = csv.reader(user_file, delimiter='|')
+        for row in reader:
+            user = User(id=row[0], email=None, password=None, age=row[1], zipcode=row[4])
+            session.add(user)
+    session.commit()
 
 def load_movies(session):
     # use u.item
@@ -13,10 +18,12 @@ def load_ratings(session):
     # use u.data
     pass
 
+
 def main(session):
     # You'll call each of the load_* functions with the session as an argument
-    pass
+    load_users(session)
+    
 
 if __name__ == "__main__":
-    s= model.connect()
+    s = model.connect()
     main(s)
